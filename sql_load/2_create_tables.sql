ce95546a86368a30c6ac45d73658673c59fe1d26
Viktoriia Urus <urusviktoriia.learn@gmail.com>
2025-08-08 19:35:53 +0200

    Update

--- sql_load/2_create_tables.sql
+++ sql_load/2_create_tables.sql
@@ -1,32 +1,32 @@
--- Таблиця користувачів
+
 CREATE TABLE public.users (
     user_id SERIAL PRIMARY KEY,
     birth_date DATE,
     sex VARCHAR(100)
 );
 
--- Таблиця продуктів
+
 CREATE TABLE public.products (
     product_id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     price NUMERIC(10, 25)
 );
 
--- Таблиця кур'єрів
+
 CREATE TABLE public.couriers (
     courier_id SERIAL PRIMARY KEY,
     birth_date DATE,
     sex VARCHAR(100)
 );
 
--- Таблиця замовлень
+
 CREATE TABLE public.orders (
     order_id SERIAL PRIMARY KEY,
     creation_time TIMESTAMP,
     product_ids INTEGER[]
 );
 
--- Таблиця дій користувачів
+
 CREATE TABLE public.user_actions (
     user_id INTEGER NOT NULL,
     order_id INTEGER,
@@ -36,7 +36,7 @@ CREATE TABLE public.user_actions (
     CONSTRAINT fk_user_actions_order FOREIGN KEY (order_id) REFERENCES public.orders(order_id)
 );
 
--- Таблиця дій кур'єрів
+
 CREATE TABLE public.courier_actions (
     courier_id INTEGER NOT NULL,
     order_id INTEGER,
@@ -53,7 +53,7 @@ ALTER TABLE public.orders OWNER TO postgres;
 ALTER TABLE public.user_actions OWNER TO postgres;
 ALTER TABLE public.courier_actions OWNER TO postgres;
 
--- Індекси для швидкості пошуку та JOIN операцій
+
 CREATE INDEX idx_user_actions_user_id ON public.user_actions(user_id);
 CREATE INDEX idx_user_actions_order_id ON public.user_actions(order_id);
 
